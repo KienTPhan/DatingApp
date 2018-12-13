b20c5e653bfa9553e85c559e1f91f5ca4a9dc4b8
Kien Phan <kienphan@Kiens-MacBook-Pro.local>
2018-12-12 21:03:03 -0600

    Initial Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+// this is a model or an entity : something that represents an entity in our database.
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id {get; set;}
+
+        public string Name { get; set;}
+    }
+}
